637241e021dd9f013dde33f531f2f43632a3a134
lloyboybebop <lloyd.a.napier@gmail.com>
2019-05-29 07:06:12 -0700

    quotes array

--- js/script.js
+++ js/script.js
@@ -14,7 +14,13 @@ project 1 - A Random Quote Generator
   Add the `year` property to at least one object in the array.
   Use console.log() to log your array of quotes to the console.
 ***/
-
+const quotes = [
+  {quote:"You should never view your challenges as a disadvantage. Instead, it's important for you to understand that your experience facing and overcoming adversity is actually one of your biggest advantages", source:"Michelle Obama", citation:"https://www.brainyquote.com/quotes/michelle_obama_791363", year:2016 },
+  {quote:"A leader is best when people barely know he exists, when his work is done, his aim fulfilled, they will say: we did it ourselves.", source:"Lao Tzu", citation:"https://www.brainyquote.com/quotes/lao_tzu_121709", year:1312},
+  {quote:"Simplicity is the glory of expression.", source:"Walt Whitman", citation:"https://www.brainyquote.com/quotes/walt_whitman_380796", year:1954 },
+  {quote:"Don't sweat the petty things and don't pet the sweaty things.", source:"George Carlin", citation:"https://www.brainyquote.com/quotes/george_carlin_383028", year:1993 },
+  {quote:"Someone asked me, if I were stranded on a desert island what book would I bring... 'How to Build a Boat.'", source:"Steven Wright", citation:"https://www.brainyquote.com/quotes/steven_wright_578936", year:1997},
+];
 
 
 
